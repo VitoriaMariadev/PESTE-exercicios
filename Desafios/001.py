9dc364c4401cc0c84c1b922454e447b87c7a23d0
VitoriaMariadev <vitoriamaria.rodrigues02@gmail.com>
2023-09-25 14:18:25 -0300

    Commit

---
+++ Desafios/001.py
@@ -0,0 +1,9 @@
+"""
+Faça uma função que receba uma lista de número inteiros e um valor aleatório. Retorne o índice de dois números onde a soma deles é igual ao valor aleatório.
+"""
+
+lista = [1, 5, 6, 7]
+
+for valores1 in lista:
+    for valores2 in lista:
+        print(valores1, valores2)
